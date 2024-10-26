8a5823a954d02e4e5363d67b0897d38ea9bebaf9
Romanriquez25 <rodrigomanriquez225@gmail.com>
2024-10-26 18:00:14 -0300

    primer Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}", // Asegúrate de incluir todas las extensiones que uses
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
